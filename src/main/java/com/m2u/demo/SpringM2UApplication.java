5a416b47bbaf20b9062be457f00c25adfb175896
sathuluri4 <54532201+sathuluri4@users.noreply.github.com>
2019-08-27 14:24:36 +0800

    Add files via upload

---
+++ src/main/java/com/m2u/demo/SpringM2UApplication.java
@@ -0,0 +1,13 @@
+package com.m2u.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringM2UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringM2UApplication.class, args);
+	}
+
+}
