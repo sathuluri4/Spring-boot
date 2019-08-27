ea9563b754f1d5c9058ff198213b96e5a941cb33
sathuluri4 <54532201+sathuluri4@users.noreply.github.com>
2019-08-27 14:26:07 +0800

    Add files via upload

---
+++ src/main/java/com/m2u/demo/model/User.java
@@ -0,0 +1,30 @@
+package com.m2u.demo.model;
+
+public class User {
+	
+	
+	private String userName;
+	private String userID;
+	private String email;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
